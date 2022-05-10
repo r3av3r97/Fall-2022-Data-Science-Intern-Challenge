3a4fe5042d4838474b93516627942282b1a1bc84
r3av3r97 <51791522+r3av3r97@users.noreply.github.com>
2022-05-10 05:56:07 -0400

    Update Shopify Internship Question 1.py

--- Shopify Internship Question 1.py
+++ Shopify Internship Question 1.py
@@ -17,13 +17,13 @@ df = pd.read_csv("2019 Winter Data Science Intern Challenge Data Set - Sheet1.cs
 # In[3]:
 
 
-df
+print(df)
 
 
 # In[4]:
 
 
-df["order_amount"].mean()
+print(df["order_amount"].mean())
 
 
 # In[5]:
@@ -41,43 +41,41 @@ print(df['order_amount'].skew())
 # In[7]:
 
 
-df.describe()[["order_amount", "total_items"]]
-
+print(df.describe()[["order_amount", "total_items"]])
 
 # In[8]:
 
 
-df.plot(x="order_id", y="order_amount", kind="line", figsize=(9, 8))
-
+print(df.plot(x="order_id", y="order_amount", kind="line", figsize=(9, 8)))
 
 # In[9]:
 
 
-df.plot.scatter(x='order_id', y='order_amount', c='DarkBlue')
+print(df.plot.scatter(x='order_id', y='order_amount', c='DarkBlue'))
 
 
 # In[10]:
 
 
-df.plot.scatter(x='order_id', y='total_items', c='DarkBlue')
+print(df.plot.scatter(x='order_id', y='total_items', c='DarkBlue'))
 
 
 # In[11]:
 
 
-df.order_amount.unique()
+print(df.order_amount.unique())
 
 
 # In[12]:
 
 
-df.plot.scatter(x='total_items', y='order_amount', c='DarkBlue')
+print(df.plot.scatter(x='total_items', y='order_amount', c='DarkBlue'))
 
 
 # In[13]:
 
 
-df.total_items.unique()
+print(df.total_items.unique())
 
 
 # In[14]:
@@ -90,26 +88,25 @@ avg_amount_per_item = df.order_amount/df.total_items
 
 
 df = df.assign(aapi = avg_amount_per_item)
-df
+print(df)
 
 
 # In[22]:
 
 
-df.describe()[["order_amount", "aapi"]]
-
+print(df.describe()[["order_amount", "aapi"]])
 
 # In[17]:
 
 
 df_filtered = df[df['order_amount'] < 1500]
-df_filtered
+print(df_filtered)
 
 
 # In[18]:
 
 
-df_filtered.total_items.unique()
+print(df_filtered.total_items.unique())
 
 
 # In[19]:
@@ -121,13 +118,13 @@ print(df_filtered['total_items'].skew())
 # In[20]:
 
 
-df_filtered.describe()[["order_amount", "total_items"]]
+print(df_filtered.describe()[["order_amount", "total_items"]])
 
 
 # In[21]:
 
 
-df_filtered.plot.scatter(x='total_items', y='order_amount', c='DarkBlue')
+print(df_filtered.plot.scatter(x='total_items', y='order_amount', c='DarkBlue'))
 
 
 # In[ ]:
